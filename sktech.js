01de334b5e7283b96d177a2e1bfce2572e9ca3d0
gianzin08 <141250733+gianzin08@users.noreply.github.com>
2023-08-09 13:57:24 -0300

    adicionando arquivos js e css

---
+++ sktech.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(600, 600);
+    background("black");
+}
+
+function draw() {
+    stroke("blue");
+    Fill("red");
+
+    1f(mouseisPressed){
+        rect(mouseX, mouseY, 20, 35);
+    }
+
+}
+
